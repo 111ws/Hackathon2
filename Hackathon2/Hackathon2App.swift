c87f9c8bf88679730b8ee6bd2dd3a51c92eb82e3
111ws <124578371+111ws@users.noreply.github.com>
2025-08-03 23:54:46 +0800

    Initial Commit

---
+++ Hackathon2/Hackathon2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon2App.swift
+//  Hackathon2
+//
+//  Created by 陆氏干饭王 on 03-08-2025.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hackathon2Tests/Hackathon2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Hackathon2Tests.swift
+//  Hackathon2Tests
+//
+//  Created by 陆氏干饭王 on 03-08-2025.
+//
+
+import Testing
+
+struct Hackathon2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
